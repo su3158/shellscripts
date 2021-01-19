6da11effd382c243929bd037c59b3ae694cec751
su3158 <su3158@protonmail.com>
2021-01-19 22:29:56 +0900

    add ssh

--- ubuntu/newmakeubuntu.sh
+++ ubuntu/newmakeubuntu.sh
@@ -4,4 +4,8 @@ echo 'run the shell script'
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
+
+sudo apt install openssh-server
+sudo apt install ufw
+sudo ufw allow openssh
 echo 'this end'
