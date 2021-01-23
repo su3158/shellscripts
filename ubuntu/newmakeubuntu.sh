b51ccb7166163910cf880d1946b1da5fca7b9be8
su3158 <su3158@protonmail.com>
2021-01-23 18:26:53 +0900

    set timezone

--- ubuntu/newmakeubuntu.sh
+++ ubuntu/newmakeubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo 'run the shell script'
+timedatectl set-timezone Asia/Tokyo
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
@@ -8,4 +10,9 @@ sudo apt autoremove -y
 sudo apt install openssh-server
 sudo apt install ufw
 sudo ufw allow openssh
+
+sudo apt install netcat
+
+alias fullupdate='sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y'
+
 echo 'this end'
