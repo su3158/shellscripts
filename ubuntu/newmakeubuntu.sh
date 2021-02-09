81796c4ca1c35c1ff23d351ac78df9a7a8552d69
su3158 <su3158@protonmail.com>
2021-02-09 20:41:34 +0900

    update ufw conf

--- ubuntu/newmakeubuntu.sh
+++ ubuntu/newmakeubuntu.sh
@@ -7,8 +7,9 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-sudo apt install openssh-server ufw netcat nginx
+sudo apt install openssh-server ufw netcat nginx git curl net-tools
 sudo ufw allow openssh
+sudo ufw allow 'Nginx Full'
 
 alias fullupdate='sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y'
 
